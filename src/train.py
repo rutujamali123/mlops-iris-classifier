f29df5f6f9ecb1744476539a9af4e6630d34cc48
rutujamali123 <rutujamali005@gmail.com>
2026-08-26 00:34:26 +0530

    feat: add classification report to training script (#1)

--- src/train.py
+++ src/train.py
@@ -8,6 +8,9 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, predctions))
 
 
 def load_data():
